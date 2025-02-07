499ce88e4da18bfec774db0384ae6899413c94a5
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2025-02-08 01:20:18 +0500

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,19 @@ import { LocationComponent } from './pages/location/location.component';
 import { ExamplesCaruselComponent } from './pages/examples-carusel/examples-carusel.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SidebarComponent } from './pages/dashboard/sidebar/sidebar.component';
+import { ProfileComponent } from './pages/dashboard/profile/profile.component';
+import { PaymentsComponent } from './pages/dashboard/payments/payments.component';
+import { HomeworkComponent } from './pages/dashboard/homework/homework.component';
+import { ExamsComponent } from './pages/dashboard/exams/exams.component';
+import { NotificationsComponent } from './pages/dashboard/notifications/notifications.component';
+import { SettingsComponent } from './pages/dashboard/settings/settings.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { HomeworkSubmitComponent } from './pages/dashboard/homework-submit/homework-submit.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -30,14 +43,27 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
     LocationComponent,
     ExamplesCaruselComponent,
     FooterComponent,
-    DashboardComponent
+    DashboardComponent,
+    SidebarComponent,
+    ProfileComponent,
+    PaymentsComponent,
+    HomeworkComponent,
+    ExamsComponent,
+    NotificationsComponent,
+    SettingsComponent,
+    HomeworkSubmitComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    FormsModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
